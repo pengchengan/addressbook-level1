e617336063bd714f98af46eb4114f2630604c50c
DESKTOP-49OHI99\user <pengchengan@u.nus.edu>
2017-01-24 07:51:22 +0800

    T2A3 Exercise 1: Reduce SLAP of method

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -208,7 +208,23 @@ public class AddressBook {
 
     public static void main(String[] args) {
         showWelcomeMessage();
-        processProgramArgs(args);
+        
+        //T2A3 Exercise 1: Reduce SLAP of method
+        //processProgramArgs(args);
+        if (args.length >= 2) {
+            showToUser(MESSAGE_INVALID_PROGRAM_ARGS);
+            exitProgram();
+        }
+
+        if (args.length == 1) {
+            setupGivenFileForStorage(args[0]);
+        }
+
+        if(args.length == 0) {
+            setupDefaultFileForStorage();
+        }
+        //T2A3 End
+        
         loadDataFromStorage();
         while (true) {
             String userCommand = getUserInput();
